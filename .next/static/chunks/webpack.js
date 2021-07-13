6595daf3b92da7e91d1ec3f565f65cdcea3ffd30
MJ <ljo094822@naver.com>
2021-07-13 10:03:25 +0900

    add: 모달창 생성중

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81976b1979a1acdfa847"; }
+/******/ 		__webpack_require__.h = function() { return "247c66a3f120faea58ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,62 @@
 import Link from 'next/link';
+import React, { useState } from 'react';
 import Layout from './components/Layout';
 import Figure from 'react-bootstrap/Figure';
+import ModalOption from './components/ModalOption';
 
 import axios from 'axios';
 
-const Index = ({ data }) => (
-  <Layout>
-    <div className="form">
-      <div className="listTitle">STORE</div>
-      <div className="lists">
-        {data.map((data) => {
-          return (
-            <Figure>
-              <Figure.Image
-                width={180}
-                height={180}
-                alt="171x180"
-                src={data.thumb}
-                style={ListStyle}
-              />
-            </Figure>
-          );
-        })}
+const Index = ({ data }) => {
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  return (
+    <Layout>
+      <div className="form">
+        <div className="listTitle">STORE</div>
+        <div className="lists">
+          {data.map((data, idx) => {
+            return (
+              <Figure key={idx}>
+                <Figure.Image
+                  width={180}
+                  height={180}
+                  alt="171x180"
+                  src={data.thumb}
+                  style={ListStyle}
+                  onClick={() => handleShow(v)}
+                />
+              </Figure>
+            );
+          })}
+        </div>
+        <ModalOption />
       </div>
-    </div>
-    <style jsx>{`
-      .form {
-        margin: 1.5rem 1.75rem;
-        text-align: center;
-      }
-      .lists {
-        display: flex;
-        margin: 1vw auto;
-        justify-content: flex-start;
-        flex-direction: row;
-        flex-wrap: wrap;
-        width: 68vw;
-      }
-      .listTitle {
-        font-family: Gotham A, Gotham B, DIN, sans-serif;
-        font-weight: 700;
-        font-size: 2rem;
-        margin-bottom: 1.75rem;
-        text-align: left;
-      }
-    `}</style>
-  </Layout>
-);
+      <style jsx>{`
+        .form {
+          margin: 1.5rem 1.75rem;
+          text-align: center;
+        }
+        .lists {
+          display: flex;
+          margin: 1vw auto;
+          justify-content: flex-start;
+          flex-direction: row;
+          flex-wrap: wrap;
+          width: 68vw;
+        }
+        .listTitle {
+          font-family: Gotham A, Gotham B, DIN, sans-serif;
+          font-weight: 700;
+          font-size: 2rem;
+          margin-bottom: 1.75rem;
+          text-align: left;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
 const ListStyle = {
   margin: '0.5vw',
   borderRadius: '5px',
@@ -62,5 +72,10 @@ Index.getInitialProps = async function () {
     data: data,
   };
 };
+
+function handleShow(breakpoint) {
+  setFullscreen(breakpoint);
+  setShow(true);
+}
 
 export default Index;
